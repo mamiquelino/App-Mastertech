52b98c16077278d8bb455087a00c20d80987072d
mamiquelino <marcela.fmiquelino@gmail.com>
2017-05-21 12:40:22 -0300

    Add files via upload

---
+++ App Mastertech/js/controllers.js
@@ -0,0 +1,44 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+    
+    $scope.dados = {
+        valor1:"",
+        valor2:"",
+        resultado:"",
+        somar: function(){
+            this.resultado = this.valor1 + this.valor2;
+        },
+        subtrair: function(){
+            this.resultado2 = this.valor1 - this.valor2;
+        }
+    }
+}])
+   
+.controller('cartTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloudTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
